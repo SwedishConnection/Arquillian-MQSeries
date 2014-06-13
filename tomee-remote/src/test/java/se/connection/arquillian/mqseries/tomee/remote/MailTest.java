085061f6cac7f8f8c6ab8818f78fa07b4c8107a5
MatthewDavidYoung <matthewdyoung@gmail.com>
2014-06-13 10:09:59 +0200

    Complete example

--- tomee-remote/src/test/java/se/connection/arquillian/mqseries/tomee/remote/MailTest.java
+++ tomee-remote/src/test/java/se/connection/arquillian/mqseries/tomee/remote/MailTest.java
@@ -1,5 +1,15 @@
 package se.connection.arquillian.mqseries.tomee.remote;
 
+import javax.jms.BytesMessage;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -8,6 +18,8 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import junit.framework.Assert;
 
 @RunWith(Arquillian.class)
 public class MailTest {
@@ -20,6 +32,51 @@ public class MailTest {
 	
 	@Test
 	public void mail() {
+		Connection qc = null;
+		Session qs = null;
+		MessageProducer sender = null;
 		
+		try {
+			InitialContext ctx = new InitialContext();
+			ConnectionFactory qcf = (ConnectionFactory) ctx.lookup("java:openejb/Resource/qcf");
+			Queue queue = (Queue) ctx.lookup("java:openejb/Resource/queue");
+			
+			qc = qcf.createConnection();
+			qs = qc.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			sender = qs.createProducer(queue);
+			
+			BytesMessage message = qs.createBytesMessage();
+			message.writeBytes(new String("Hello").getBytes());
+			
+			sender.send(message);
+		} catch (NamingException e) {
+			Assert.fail();
+		} catch (JMSException e) {
+			Assert.fail();
+		}
+		finally {
+			if (sender != null) {
+				try {
+					sender.close();
+				} catch (JMSException e) {
+					Assert.fail();
+				}
+			}
+			if (qs != null) {
+				try {
+					qs.close();
+				} catch (JMSException e) {
+					Assert.fail();
+				}
+			}
+			if (qc != null) {
+				try {
+					qc.close();
+				} catch (JMSException e) {
+					Assert.fail();
+				}
+			}
+		}
 	}
 }
